23b198151db1083a15f83d67203b9075e2356f8d
Andrew Johnson <manilda7@gmail.com>
2019-10-03 09:57:37 -0600

    Adds new routes for finding

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
       resources :items, only: [:index, :show]
+      namespace :customers do
+        get '/find', to: 'search#show'
+      end
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
+      namespace :invoice_items do
+        get '/find', to: 'search#show'
+      end 
       resources :invoice_items, only: [:index, :show]
       resources :transactions, only: [:index, :show]
     end
