8951776c3d5622335ee135f1da408a699c2f3c57
Andrew Johnson <manilda7@gmail.com>
2019-10-03 11:01:15 -0600

    Adds new routes for finding one instance of each resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,29 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
       resources :merchants, only: [:index, :show]
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
       resources :items, only: [:index, :show]
       namespace :customers do
         get '/find', to: 'search#show'
       end
       resources :customers, only: [:index, :show]
+      namespace :invoices do
+        get '/find', to: 'search#show'
+      end
       resources :invoices, only: [:index, :show]
       namespace :invoice_items do
         get '/find', to: 'search#show'
-      end 
+      end
       resources :invoice_items, only: [:index, :show]
+      namespace :transactions do
+        get '/find', to: 'search#show'
+      end 
       resources :transactions, only: [:index, :show]
     end
   end
