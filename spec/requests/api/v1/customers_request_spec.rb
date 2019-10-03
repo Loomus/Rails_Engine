4b4d281ea259a69d8a7974f5229274e51cc27764
Andrew Johnson <manilda7@gmail.com>
2019-10-03 09:55:22 -0600

    Adds test to find a single customer

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -23,4 +23,16 @@ describe "Customer API" do
     expect(response).to be_successful
     expect(customer["id"]).to eq(id)
   end
+
+  it "can find one customer by first name" do
+    first_name = create(:customer).first_name
+
+    get "/api/v1/customers/find?first_name=#{first_name}"
+
+    expect(response).to be_successful
+
+    customer = JSON.parse(response.body)
+    
+    expect(customer['data']['attributes']['first_name']).to eq(first_name)
+  end
 end
